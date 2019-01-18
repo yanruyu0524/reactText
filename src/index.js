204e0e50184ae2965de134ccb113817322dfb844
yanruyu <1670328067.com>
2019-01-18 20:08:53 +0800

    模板

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from 'view/App'
+import 'common/css/style.scss'
+import './common/css/swiper.min.css'
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+
+
+
